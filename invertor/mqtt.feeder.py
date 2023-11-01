c22dc489a9318918d07bb61a60634307ad2d650f
karotka <karotka@seznam.cz>
2023-11-01 17:00:46 +0100

    xxx

--- invertor/mqtt.feeder.py
+++ invertor/mqtt.feeder.py
@@ -11,11 +11,12 @@ from influxdb import DataFrameClient
 import paho.mqtt.client as mqtt 
 import json
 import numpy as np
+import traceback
 
 broker_address="192.168.0.224"
 pidfile = "/tmp/mqtt.feeder.pid"
 
-mqttClient = mqtt.Client("P1") #create new instance
+mqttClient = mqtt.Client("feeder") #create new instance
 
 class Encoder(json.JSONEncoder):
     def default(self, obj):
@@ -82,30 +83,47 @@ def write(dt, period):
 
     if period  == "hour":
         # get all months
-        df = client.query("select * from invertor_monthly")
+        df = client.query("select solarPowerIn from invertor_monthly where time > now() - 365d")
         #dataDict["columns"] = df["invertor_monthly"].columns.values
         dataDict["values"] = df["invertor_monthly"].values
-        mqttClient.publish("home/invertor/0/monthly/rows/", json.dumps(dataDict, cls=Encoder), qos=1, retain=True)
+        mqttClient.publish("home/invertor/monthly/rows/", json.dumps(dataDict, cls=Encoder), qos=1, retain=True)
 
     elif period  == "minute":
         # get last x days
-        df = client.query("select sum(batteryPowerIn) as batteryPowerIn, sum(batteryPowerOut) as batteryPowerOut,  sum(outputPowerActive) as outputPowerActive, sum(outputPowerApparent) as outputPowerApparent, sum(solarPowerIn) as solarPowerIn from invertor_daily where time > now() - 9d group by time(1d)")
+        df = client.query("select sum(batteryPowerIn) as batteryPowerIn, sum(batteryPowerOut) as batteryPowerOut,  sum(outputPowerActive) as outputPowerActive, sum(outputPowerApparent) as outputPowerApparent, sum(solarPowerIn) as solarPowerIn from invertor_daily where time > now() - 13d group by time(1d)")
         dataDict["columns"] = df["invertor_daily"].columns.values
-        dataDict["values"] = df["invertor_daily"].fillna(0).values
-        mqttClient.publish("home/invertor/0/daily/rows/", json.dumps(dataDict, cls=Encoder), qos=1, retain=True)
+        dataDict["values"] = df["invertor_daily"].fillna(0).values[1:]
+        mqttClient.publish("home/invertor/daily/rows/", json.dumps(dataDict, cls=Encoder), qos=1, retain=True)
 
     else:
-        # get invertor status
-        df = client.query("select * from invertor_status order by time desc limit 1")
-        dataDict["status"] = df["invertor_status"].iloc[0].to_dict()
-        # actual
-        df = client.query("select *  from invertor_actual order by time desc limit 1")
-        dataDict["actual"] = df["invertor_actual"].iloc[0].to_dict()
+        try:
+            # get invertor status
+            df = client.query("select * from invertor_status order by time desc limit 1")
+            dataDict["status"] = df["invertor_status"].iloc[0].to_dict()
+        
+            # actual string 1
+            df1 = client.query("select batteryCurrent, batteryDischargeCurrent, batteryVoltage, batteryVoltageSCC, busVoltage, deviceNumber, loadPercent, outputFreq, outputPowerActive, outputPowerApparent, outputVoltage, solarCurrent, solarVoltage, temperature, gridFreq, gridVoltage from invertor_actual where deviceNumber = 'first' order by time desc limit 1")
 
-        mqClient.publish("home/invertor/0/actual/", json.dumps(dataDict, cls=Encoder), qos=1, retain=True)
+            # actual string 2
+            df2 = client.query("select batteryCurrent, batteryDischargeCurrent, batteryVoltage, batteryVoltageSCC, busVoltage, deviceNumber, loadPercent, outputFreq, outputPowerActive, outputPowerApparent, outputVoltage, solarCurrent, solarVoltage, temperature, gridFreq, gridVoltage from invertor_actual where deviceNumber = 'second' order by time desc limit 1")
 
-    logging.info("Send <%s> data to mqtt broker ok time: %s" % (period, dt))
+            try:
+                bc1 = df1["invertor_actual"].batteryCurrent
+                bc2 = df2["invertor_actual"].batteryCurrent
 
+                dataDict["invertor2"] = df2["invertor_actual"].iloc[0].to_dict()
+                dataDict["invertor1"] = df1["invertor_actual"].iloc[0].to_dict()
+            except Exception as e:
+                logging.error("%s" % (e, ))
+                return
+
+            mqttClient.publish("home/invertor/actual/", json.dumps(dataDict, cls=Encoder), qos=1, retain=True)
+        
+            logging.info("Send <%s> data to mqtt broker ok" % (period, ))
+
+        except Exception as e:
+            logging.error("Exception: %s" % (traceback.format_exc(), ))
+        
 
 oldMinute = 0
 oldHour = 0
@@ -132,4 +150,8 @@ except Exception as e:
 
 finally:
     os.unlink(pidfile)
+
+
+
+
 
